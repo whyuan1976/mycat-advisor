bd7f5cacbee2b3bab5d192c0260950ed098aed29
newboy2004 <newboy2004@126.com>
2016-04-05 10:30:27 +0800

    订单模块完善

--- persistence/src/main/java/org/apache/mycat/advisor/persistence/dao/TabOrderMapper.java
+++ persistence/src/main/java/org/apache/mycat/advisor/persistence/dao/TabOrderMapper.java
@@ -1,8 +1,13 @@
 package org.apache.mycat.advisor.persistence.dao;
 
+import java.util.List;
+
 import org.apache.mycat.advisor.persistence.model.TabOrder;
 import org.apache.mycat.advisor.persistence.util.MyMapper;
 
 public interface TabOrderMapper extends MyMapper<TabOrder> {
+	
+	 public List<TabOrder> listByType(TabOrder tabOrder);
 
+	public TabOrder getOrderInfoById(long id);
 }
